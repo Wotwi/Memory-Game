e9cb27cbe20c704c1ce416643944d40a684d8bfc
Jakub Wawrzyn <j.wawrzyn99@gmail.com>
2024-10-16 13:17:28 +0200

    added new style files and new component - Start.jsx

---
+++ src/components/Start.jsx
@@ -0,0 +1,16 @@
+function Start() {
+
+    return (
+      <>
+        <div className="start__container">
+            <h1 className="start__header">Zagraj w Memory!</h1>
+            <h5 className="start__subheader">Zanim zaczniesz, przeczytaj instrukcję.</h5>
+            <p className="start__instruction">Znajdź dwa identyczne obrazki odsłaniając elementy, jeden po drugim.<br /> Spróbuj znaleźć wszystkie w jak najkrótszym czasie.</p>
+            <button className="start__btn">START</button>
+        </div>
+      </>
+    )
+  }
+  
+  export default Start
+  
